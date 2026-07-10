ae804a95ddde98d0edc6c70594538de4e7f335c4
james <kangjaeku.g@gmail.com>
2026-07-10 10:40:33 +0900

    v1.4

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080;
 
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from v1.3!\n');
+  res.end('Hello from v1.4!\n');
 }).listen(port);
 
 console.log(`Server running on port ${port}`);
